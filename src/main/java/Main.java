f9f91cc348145d2a3b4cf48d8feb2672fa77fe86
Fegao333 <karnausenkosergej19@gmail.com>
2026-07-22 17:24:39 +0300

    работа над ошибками

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,52 +3,59 @@ import java.util.Scanner;
 //пишу этот комментарий до начала работы, сразу скажу без Ии я врят-ли обойдусь, буду использовать Deep Seek там где не хватает моей двух недельной квалификации, но буду это комментировать как и весь код в целом
 public class Main {
     public static void main(String[] args) {
-        Raсe race = new Raсe();
-        Scanner scanner = new Scanner(System.in);
-        Car[] cars = new Car[3]; // Идею использования массива Car подсмотрел у Ии, изначально я хотел использовать ArrayList, но в нем нет нужды т.к. у нас фиксированные вводные данные и лишний раз с ним возиться не хочу :P
-        for (int i = 0; i < cars.length; i++) {
+        Race race = new Race();
+        Scanner scanner = new Scanner(System.in); //удалил как оказывается не нужный массив
+        for (int i = 0; i < 3; i++) {
             System.out.println("— Введите название машины №" + (i + 1) + ": ");
             String mark = scanner.nextLine();
             int speed = 0;
-            while (true) {
+            boolean isValid = false;//реализовал цикл по вашей рекомендации по другому, теперь это while цикл в котором для выхода используется значение boolean, это подняло и читаемость и безопасность цикла, теперь в нем сложнее накосячить
+            while (!isValid) {
                 System.out.print("— Введите скорость машины №" + (i + 1) + ": ");
-                speed = scanner.nextInt();
-                scanner.nextLine();
-                if (speed > 0 && speed < 251) {
-                    break;
-                } else {
-                    System.out.println("— Неправильная скорость");
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    scanner.nextLine();
+                    final int minSpeed = 0; //вынес значения за цикл if, а в самом цикле оставил только переменные, так же на всякий случай тоже сделал их final что бы нельзя было изменить
+                    final int maxSpeed = 250;
+                    if (speed > minSpeed && speed <= maxSpeed) {
+                        isValid = true;
+                    } else {
+                        System.out.println("— Неправильная скорость");
+                    }
+                }  else { System.out.println("— Ошибка! Введите число.");
+                    scanner.nextLine();}// добавил проверку на то что пишет пользователь в графу скорости, теперь если писать буквы то он просто выдаст ошибку
+
                 }
-
+                Car currentCar = new Car(mark, speed);
+                race.updateLeader(currentCar);
             }
-            cars[i] = new Car(mark, speed);
-            race.updateLeader(cars[i]);
+            System.out.println("Лидер гонки: '" + race.leader + "'");
         }
-        System.out.println("Лидер гонки: '" + race.leader +"'");
-    }
-}
-class Car {
-    String mark;
-    int speed;
-    public Car(String mark, int speed) {
-        this.mark = mark;
-        this.speed = speed;
     }
 
+     class Car {
+        final String mark;//подкорректировал
+        final int speed;
+
+        public Car(String mark, int speed) {
+            this.mark = mark;
+            this.speed = speed;
+        }
 
 
+    }
 
+     class Race {
+        String leader = "";
+        int leaderDistance = 0;
 
-}
-class Raсe {
-    String leader = "";
-    int leaderDistance = 0;
-    public void updateLeader(Car car) {
-        int time = 24;
-        int distance = car.speed * time;
-        if (distance >leaderDistance ) {
-            leader = car.mark;
-            leaderDistance = car.speed * time;}
+        public void updateLeader(Car car) {
+            int time = 24;
+            int distance = car.speed * time;
+            if (distance > leaderDistance) {
+                leader = car.mark;
+                leaderDistance = car.speed * time;
+            }
 
-}
+        }
     }
